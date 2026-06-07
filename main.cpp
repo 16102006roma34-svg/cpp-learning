c11b03fbe8ca9c64469823a3766a29287d095a0b
CITRUC6 <16102006roma34@gmail.com>
2026-06-08 02:29:40 +0500

    работа с классами №4

--- main.cpp
+++ main.cpp
@@ -1,65 +1,114 @@
-#include <map>
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
+class Account {
+    private: 
+    unsigned int id;
+    string owner;
+    float balance; //я подумал лучше баланс сделать флоут 
 
-
-class Translator {
-private: 
-    map <string, string> slovar;
-public:
-    void addTranslate(const string& keyword, const string& translate) {
-        auto it=slovar.find(keyword);
-
-        if (it!=slovar.end()) {
-            cout << "перевод слова " << keyword << " уже записан как " << it->second << endl;
-            cout << "Хотите заменить перевод? 1 - да, 0 - нет";
-            bool hochu;
-            cin >> hochu;
-            cin.ignore();
-            if (!hochu)
-                return;
-        }
-        slovar[keyword]=translate;
-    }
-
-    void printTranslate(const string& keyword) const {
-        auto it=slovar.find(keyword);
-
-        if(it!=slovar.end()) {
-            cout << "Перевод слова " << keyword << ": " << it->second << endl;
+    public:
+    void setOwner(const string& newOwner) {
+        if (newOwner.empty()) {
+            owner="Пусто";
         }
         else {
-            cout << "Слово " << keyword << " не найдено в словаре" << endl;
+            owner=newOwner;
+        }
+        cout << "Новое имя владельца счёта: " << owner << endl;
+    }
+
+    void setBalance(float newBalance) {
+        if (newBalance<0) {
+            balance=0;
+            cout << "Вы ввели отрицательное число." << endl;
+        }
+        else {
+            balance=newBalance;
+        }
+        cout << "Текущий баланс: " << balance << endl;
+    }
+
+    unsigned int getId() const {
+        return id;
+    }
+
+    string getOwner() const {
+        return owner;
+    }
+
+    float getBalance() const {
+        return balance;
+    }
+
+    void deposit(int amount) {
+        if (amount<=0) {
+            cout << "Баланс не может быть увеличен на отрицательное значение" << endl;
+            return;
+        }
+        else {
+            balance+=amount;
+        }
+    }
+ 
+    bool withdraw(int amount) {
+        if (balance<amount) {
+            cout << "На счёте недостаточно средств";
+            return false;
+        }
+        else {
+            cout << "Транзакция выполнена. Со счёта списано " << amount << "рублей." << endl << "На балансе осталось " << balance << "рублей"; 
         }
     }
 
-    void printAll() const {
-        cout << "Полный словарь: " << endl ;
-        if (slovar.empty()) {
-            cout << "Словарь пуст!" << endl;
-            return;
-        }
-        for (const auto& [key, value] : slovar) {
-            cout <<  key << "->" << value << endl;
-        }
+    void print() const {
+        cout << "ФИО: " << getOwner() << ". Баланс: " << getBalance() << " руб." << endl;
+    }
+
+    Account(unsigned int newId, const string& newOwner, float newBalance): id(newId), owner(newOwner), balance(newBalance) {
+      id=newId;
+      setBalance(newBalance);
+      setOwner(newOwner);  
     }
 };
 
+class BankProceccor {
+private:
+    vector<Account> Accounts_bd;
+    unsigned int nextId=1;
 
-int main() {
-    Translator slovar;
+public:
 
-    slovar.addTranslate("apple", "яблоко");
-    slovar.addTranslate("banana","банан");
-    slovar.addTranslate("potato","картопля");
+    void registerAccount(const string& newOwner, float newBalance) {
+        Accounts_bd.push_back(Account(nextId++, newOwner, newBalance));
+        cout << "В базу данных добавлена новая учетная запись.\n ФИО:" << newOwner << ". Баланс: " << newBalance << endl; 
+    }
 
-    string keyword;
+    void showAllAccounts() const {
+        if (Accounts_bd.empty()) {
+            cout << "База данных пуста." << endl;
+        }
+        else {
+        for (const Account& account : Accounts_bd) {
+            account.print();
+        }
+        }
+    }
 
-    getline(cin, keyword);
-
-    slovar.printTranslate(keyword);
-
-    slovar.printAll();
-}
+    void processTransaction(int processId, int amount, bool isDeposit) {
+     for (Account& account : Accounts_bd) {
+        if (account.getId()==processId) {
+            if(isDeposit) {//ну вот тут пиздец короче. понимаешь наверн о чём я 
+                account.deposit(amount);
+                return;
+            }
+            else {
+                account.withdraw(amount);
+                return;
+            }            
+        }
+     }   
+     cout << "Клиент с ID " << processId << " не найден" << endl; 
+    }
+};
