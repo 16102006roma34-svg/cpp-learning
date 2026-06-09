1c817b0eb6982f567c729e23ca016fe2484c28a5
CITRUC6 <16102006roma34@gmail.com>
2026-06-09 16:37:08 +0500

    работа с классами №5+map

--- main.cpp
+++ main.cpp
@@ -1,114 +1,60 @@
-#include <iostream>
+#include <iostream> 
+#include <map>
 #include <string>
-#include <vector>
 using namespace std;
-class Account {
-    private: 
-    unsigned int id;
-    string owner;
-    float balance; //я подумал лучше баланс сделать флоут 
+class Product {
+    string name;
+    unsigned int quantity; 
 
     public:
-    void setOwner(const string& newOwner) {
-        if (newOwner.empty()) {
-            owner="Пусто";
-        }
-        else {
-            owner=newOwner;
-        }
-        cout << "Новое имя владельца счёта: " << owner << endl;
+    Product(const string& newName, unsigned int newQuantity) : name(newName), quantity(newQuantity) {
     }
 
-    void setBalance(float newBalance) {
-        if (newBalance<0) {
-            balance=0;
-            cout << "Вы ввели отрицательное число." << endl;
-        }
-        else {
-            balance=newBalance;
-        }
-        cout << "Текущий баланс: " << balance << endl;
+    Product():name(""), quantity(0){}
+    
+    void changeQuantity(int amount) {
+        quantity+=amount;
     }
 
-    unsigned int getId() const {
-        return id;
+    const string& getName () const {
+        return name; 
     }
 
-    string getOwner() const {
-        return owner;
-    }
-
-    float getBalance() const {
-        return balance;
-    }
-
-    void deposit(int amount) {
-        if (amount<=0) {
-            cout << "Баланс не может быть увеличен на отрицательное значение" << endl;
-            return;
-        }
-        else {
-            balance+=amount;
-        }
-    }
- 
-    bool withdraw(int amount) {
-        if (balance<amount) {
-            cout << "На счёте недостаточно средств";
-            return false;
-        }
-        else {
-            cout << "Транзакция выполнена. Со счёта списано " << amount << "рублей." << endl << "На балансе осталось " << balance << "рублей"; 
-        }
+    unsigned int getQuantity() const {
+        return quantity;
     }
 
     void print() const {
-        cout << "ФИО: " << getOwner() << ". Баланс: " << getBalance() << " руб." << endl;
-    }
-
-    Account(unsigned int newId, const string& newOwner, float newBalance): id(newId), owner(newOwner), balance(newBalance) {
-      id=newId;
-      setBalance(newBalance);
-      setOwner(newOwner);  
+        cout << "Название товара: " << name << ". Количество на складе: " << quantity << endl;
     }
 };
 
-class BankProceccor {
-private:
-    vector<Account> Accounts_bd;
-    unsigned int nextId=1;
-
-public:
-
-    void registerAccount(const string& newOwner, float newBalance) {
-        Accounts_bd.push_back(Account(nextId++, newOwner, newBalance));
-        cout << "В базу данных добавлена новая учетная запись.\n ФИО:" << newOwner << ". Баланс: " << newBalance << endl; 
+class Warehouse {
+    map <unsigned int, Product> database;
+    int nextCode=101;
+    
+    public:
+    void addProduct(const string& newName, unsigned int newQuantity) {
+        database[nextCode++]=Product(newName, newQuantity);
     }
 
-    void showAllAccounts() const {
-        if (Accounts_bd.empty()) {
-            cout << "База данных пуста." << endl;
+    bool updateStock(int barcode, unsigned int count) {
+        auto it=database.find(barcode);
+
+        if(it==database.end()) {
+            return false;
         }
         else {
-        for (const Account& account : Accounts_bd) {
-            account.print();
-        }
+            it->second.changeQuantity(count);
+            return true;
         }
     }
 
-    void processTransaction(int processId, int amount, bool isDeposit) {
-     for (Account& account : Accounts_bd) {
-        if (account.getId()==processId) {
-            if(isDeposit) {//ну вот тут пиздец короче. понимаешь наверн о чём я 
-                account.deposit(amount);
-                return;
-            }
-            else {
-                account.withdraw(amount);
-                return;
-            }            
+    void printAll() const {
+        for (const auto & [barcode, product]:database) {
+            cout << "[" << barcode << "] ";
+            product.print();
         }
-     }   
-     cout << "Клиент с ID " << processId << " не найден" << endl; 
     }
 };
+
