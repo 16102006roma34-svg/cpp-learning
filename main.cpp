37559412a8d889e0a2c0b9a0d55b0443a13b0924
CITRUC6 <16102006roma34@gmail.com>
2026-06-11 20:15:11 +0500

    8: научился выдавать сообщение об ошибке через runtime

--- main.cpp
+++ main.cpp
@@ -1,85 +1,43 @@
-#include <vector> 
-#include <map> 
 #include <iostream>
-#include <string>
+#include <vector> 
+#include <map>
 #include <memory>
-using namespace std;
+#include <stdexcept>
 
-class Server {
-    string name;
-    string ip;
-    vector <string> logs;
-
-    public:
-    const string& getName() {
-        return name;
-    }
-
-    const string& getIp() {
-        return ip;
-    }
-
-    void addLog(const string& log) {
-        logs.push_back(log);
-    }
-
-    void printServer() const {
-        cout << "Название сервера: " << name << endl;
-        cout << "IP-адрес: " << ip << endl;
-        cout << "Список логов: " << endl;
-        for (const string& tempLog : logs) {
-            cout << tempLog << endl;
-        }
-    }
-
-    Server(string newName, string newIp):name(newName), ip(newIp) {
-
-    }
-
-    Server() : name(""), ip("") {}
-};
-
-class ClusterManager {
-    unsigned int nextId=1;
-    map <unsigned int, unique_ptr<Server>> database;
+class AuthManager {
+    private: 
+    std::map <std::string, std::string> users;
 
     public:
-    bool AddServer (const string& newName, const string& newIp) {
-        database[nextId++]=make_unique<Server>(newName, newIp);
-        return true;
-    }
-    
-
-    bool addLogToServer(unsigned int serverId, const string& newLog) {
-        auto it=database.find(serverId);
-        if (it==database.end()) 
-            return false;
-        else {
-            it->second->addLog(newLog);
-            return true;
-        } 
+    AuthManager() {
+        users["admin"]="1234";
+        users["alex"]="qwerty";
     }
 
-    void printReport() const {
-        for (const auto& [curId, server]:database) {
-            cout << "Все сервера:\n";
-            cout << "[" << curId << "] ";
-            server->printServer(); 
+    void login(const std::string& username, const std::string& password) {
+        auto it=users.find(username);
+        if (it==users.end())
+            throw std::runtime_error("Ошибка: пользователь " + username + "не найден");
+        else if (it->second!=password) {
+            throw std::runtime_error("Ошибка: пароль введён неверно");
         }
-    }
+    }   
 };
 
 int main() {
-    ClusterManager manager;
-    Server server4("server4", "8.8.8.8");
-    Server server5("server5", "8.8.4.4");
-    Server server6("server6", "192.168.31.8");
-    string log1="абуагагагагагга";
-    
-    server4.addLog(log1);
-    server4.printServer();
+    AuthManager authmanager;
 
-    manager.AddServer("server4", "4.4.4.4");
-    cout << manager.addLogToServer(1, log1) << endl; //ничего, что айди я просто выдумал? подразумевается же что раз надо добавить лог то программист айди сервера куда добавляем уже знает
-    cout << manager.addLogToServer(2, log1) << endl;
+
+    std::string login;
+    getline(std::cin, login);
+    std::string password;
+    getline(std::cin, password);
+
+    try {
+    authmanager.login(login, password);
+    }
+    catch (std::runtime_error& error) {
+        std::cout << "Перехвачено исключение. Причина:\n";
+        std::cout << error.what(); 
+    }
 }
